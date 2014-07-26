98704bddc760df39f3b615d5683f2773e1c235b8
Rahul Akurati <rahul.akurati@gmail.com>
2014-07-26 10:59:18 -0700

    code commit
    commit all the code

---
+++ imported/20_CandidateMasterDetail_Interviewed_psd/layers.json.js
@@ -0,0 +1,728 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["20_CandidateMasterDetail_Interviewed_psd/layers.json.js"] = [
+	{
+		"id": 593,
+		"name": "bg",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/bg.png",
+			"frame": {
+				"x": 0,
+				"y": 393,
+				"width": 4269,
+				"height": 6676
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1820354933"
+	},
+	{
+		"id": 110,
+		"name": "panel2",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/panel2.png",
+			"frame": {
+				"x": 1535,
+				"y": 1298,
+				"width": 2668,
+				"height": 2355
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "469198139"
+	},
+	{
+		"id": 583,
+		"name": "cmore",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/cmore.png",
+			"frame": {
+				"x": 3952,
+				"y": 3598,
+				"width": 248,
+				"height": 44
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1753246975"
+	},
+	{
+		"id": 180,
+		"name": "panel3",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/panel3.png",
+			"frame": {
+				"x": 1534,
+				"y": 3743,
+				"width": 2690,
+				"height": 2877
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1539717113"
+	},
+	{
+		"id": 585,
+		"name": "rsm",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/rsm.png",
+			"frame": {
+				"x": 1536,
+				"y": 6686,
+				"width": 486,
+				"height": 150
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1753336412"
+	},
+	{
+		"id": 581,
+		"name": "panel1",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/panel1.png",
+			"frame": {
+				"x": 1469,
+				"y": 825,
+				"width": 2800,
+				"height": 600
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "119054347"
+	},
+	{
+		"id": 591,
+		"name": "magicline",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/magicline.png",
+			"frame": {
+				"x": 1533,
+				"y": 1392,
+				"width": 371,
+				"height": 34
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1753366171"
+	},
+	{
+		"id": 250,
+		"name": "Resume",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Resume.png",
+			"frame": {
+				"x": 3739,
+				"y": 1316,
+				"width": 211,
+				"height": 43
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1753366264"
+	},
+	{
+		"id": 246,
+		"name": "Network",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Network.png",
+			"frame": {
+				"x": 3387,
+				"y": 1300,
+				"width": 281,
+				"height": 69
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1941377639"
+	},
+	{
+		"id": 242,
+		"name": "Experience",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Experience.png",
+			"frame": {
+				"x": 2964,
+				"y": 1294,
+				"width": 352,
+				"height": 76
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "77874614"
+	},
+	{
+		"id": 229,
+		"name": "Qualification",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Qualification.png",
+			"frame": {
+				"x": 1975,
+				"y": 1298,
+				"width": 384,
+				"height": 69
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1715752401"
+	},
+	{
+		"id": 226,
+		"name": "Overview",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Overview.png",
+			"frame": {
+				"x": 1603,
+				"y": 1298,
+				"width": 304,
+				"height": 69
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "618791252"
+	},
+	{
+		"id": 234,
+		"name": "Team Feedback",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Team Feedback.png",
+			"frame": {
+				"x": 2427,
+				"y": 1300,
+				"width": 477,
+				"height": 69
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1165856487"
+	},
+	{
+		"id": 566,
+		"name": "leftpanel",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/leftpanel.png",
+			"frame": {
+				"x": 0,
+				"y": 825,
+				"width": 1470,
+				"height": 6244
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "692865641"
+	},
+	{
+		"id": 490,
+		"name": "lastpanel",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/lastpanel.png",
+			"frame": {
+				"x": 0,
+				"y": 5071,
+				"width": 1465,
+				"height": 540
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "308685147"
+	},
+	{
+		"id": 714,
+		"name": "footer",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/footer.png",
+			"frame": {
+				"x": 0,
+				"y": 6902,
+				"width": 4267,
+				"height": 167
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1754230203"
+	},
+	{
+		"id": 710,
+		"name": "teamfb",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/teamfb.png",
+			"frame": {
+				"x": 204,
+				"y": 5368,
+				"width": 919,
+				"height": 43
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1754230264"
+	},
+	{
+		"id": 712,
+		"name": "missing",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/missing.png",
+			"frame": {
+				"x": 203,
+				"y": 5465,
+				"width": 643,
+				"height": 57
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1754230266"
+	},
+	{
+		"id": 371,
+		"name": "Compare",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Compare.png",
+			"frame": {
+				"x": 1035,
+				"y": 6902,
+				"width": 358,
+				"height": 166
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "2054582537"
+	},
+	{
+		"id": 366,
+		"name": "Reject",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Reject.png",
+			"frame": {
+				"x": 3997,
+				"y": 6955,
+				"width": 202,
+				"height": 69
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1754259866"
+	},
+	{
+		"id": 376,
+		"name": "makeoffer",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/makeoffer.png",
+			"frame": {
+				"x": 3429,
+				"y": 6902,
+				"width": 498,
+				"height": 167
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1180738735"
+	},
+	{
+		"id": 11,
+		"name": "header",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/header.png",
+			"frame": {
+				"x": 2,
+				"y": 0,
+				"width": 4267,
+				"height": 834
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "607009337"
+	},
+	{
+		"id": 67,
+		"name": "New",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/New.png",
+			"frame": {
+				"x": 162,
+				"y": 350,
+				"width": 475,
+				"height": 344
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1563115313"
+	},
+	{
+		"id": 73,
+		"name": "Interviewingd",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Interviewingd.png",
+			"frame": {
+				"x": 664,
+				"y": 350,
+				"width": 475,
+				"height": 344
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "529394347"
+	},
+	{
+		"id": 76,
+		"name": "Interviewed",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Interviewed.png",
+			"frame": {
+				"x": 1167,
+				"y": 350,
+				"width": 475,
+				"height": 344
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1848345460"
+	},
+	{
+		"id": 79,
+		"name": "On Hold",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/On Hold.png",
+			"frame": {
+				"x": 1669,
+				"y": 350,
+				"width": 475,
+				"height": 344
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1251304092"
+	},
+	{
+		"id": 82,
+		"name": "Made Offer",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Made Offer.png",
+			"frame": {
+				"x": 2171,
+				"y": 350,
+				"width": 475,
+				"height": 344
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "136885579"
+	},
+	{
+		"id": 85,
+		"name": "Accepted",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Accepted.png",
+			"frame": {
+				"x": 3137,
+				"y": 350,
+				"width": 475,
+				"height": 344
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1882007782"
+	},
+	{
+		"id": 88,
+		"name": "Rejected",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 4271,
+			"height": 7069
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Rejected.png",
+			"frame": {
+				"x": 3639,
+				"y": 350,
+				"width": 475,
+				"height": 344
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "786379229"
+	}
+]
